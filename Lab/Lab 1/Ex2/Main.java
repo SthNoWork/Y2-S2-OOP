0c87efbfd62fe4548e52a4219f950708d19d1b7b
Hengrith <rith.lor@gmail.com>
2026-03-19 00:42:20 +0700

    eh

--- Lab/Lab 1/Ex2/Main.java
+++ Lab/Lab 1/Ex2/Main.java
@@ -1,18 +1,17 @@
 package Ex2;
 
 public class Main {
-        public static void main(String[] args) {
-        System.out.printf("%-15s %-15s %n", "\\n","Line break.");
-        System.out.printf("%-15s %-15s %n", "\\t","Tabulation.");
-        System.out.printf("%-15s %-15s %n", "\\'","Single Quote.");
-        System.out.printf("%-15s %-15s %n", "\\\"","Double Quote.");
-        System.out.printf("%-15s %-15s %n", "\\\\","\\ Sign.");
-        System.out.printf("%-15s %-15s %n", "\\\\\\\\","\\\\ Sign.");
-        System.out.printf("%-15s %-15s %n", "//","Line Comment.");
-        System.out.printf("%-15s %-15s %n", "/* ... */","Block Comment.");
+    public static void main(String[] args) {
+        System.out.printf("%-15s %-15s %n", "\\n", "Line break.");
+        System.out.printf("%-15s %-15s %n", "\\t", "Tabulation.");
+        System.out.printf("%-15s %-15s %n", "\\'", "Single Quote.");
+        System.out.printf("%-15s %-15s %n", "\\\"", "Double Quote.");
+        System.out.printf("%-15s %-15s %n", "\\\\", "\\ Sign.");
+        System.out.printf("%-15s %-15s %n", "\\\\\\\\", "\\\\ Sign.");
+        System.out.printf("%-15s %-15s %n", "//", "Line Comment.");
+        System.out.printf("%-15s %-15s %n", "/* ... */", "Block Comment.");
         System.out.printf("%-15s %n", "\"\"\"");
-        System.out.printf("%-15s %-15s %n", "","Text block.");
+        System.out.printf("%-15s %-15s %n", "", "Text block.");
         System.out.printf("%-15s %n", "\"\"\"");
     }
 }
-t
