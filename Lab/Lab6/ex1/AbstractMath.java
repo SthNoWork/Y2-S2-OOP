d5f7315b883c6f19b7c536a67b3b78ca374d48f3
Hengrith <rith.lor@gmail.com>
2026-04-20 15:04:29 +0700

    0.5 done lmao

---
+++ Lab/Lab6/ex1/AbstractMath.java
@@ -0,0 +1,16 @@
+class AbstractMath {
+    static int reverseNumber(int num) {
+        return 0;
+    }
+    static int roundedNumber(double num) {
+        return 0;
+    }
+
+    static int countDistinct(int[] values) {
+        return 0;
+    }
+
+    static int decimalToBinary(int decimal) {
+        return 0;
+    }
+}
--- Lab/Lab6/ex1/Main.java
+++ Lab/Lab6/ex1/Main.java
@@ -1,6 +1,82 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
-        
+        Scanner input = new Scanner(System.in);
+        int option;
+        while (true) {
+            System.out.println("==== Menu ===");
+            System.out.println("1. Reverse a number");
+            System.out.println("2. Rounded number");
+            System.out.println("3. Count distinct number");
+            System.out.println("4. Decimal to Binary");
+            System.out.println("5. Quit");
+            System.out.println("Choose an opt: ");
+            try {
+                option = Integer.parseInt(input.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Error!");
+                continue;
+            }
+
+            if (option == 5) {
+                System.out.println("Goodbye!");
+                break;
+            }
+
+            switch (option) {
+                case 1:
+                    reverseNumber(input);
+                    break;
+                case 2:
+                    roundedNumber(input);
+                    break;
+                case 3:
+                    countDistinct(input);
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Error!");
+                    break;
+            }
+        }
     }
+
+    static void reverseNumber(Scanner input) {
+        int num;
+        System.out.println("::: Reverse a number :::");
+        System.out.println("Input number: ");
+        try {
+            num = Integer.parseInt(input.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Error!");
+            continue;
+        }
+        System.out.println("Output is: ");
+        AbstractMath.reverseNumber(option);
+    }
+
+    static void roundedNumber(Scanner input) {
+        System.out.println("::: Reverse a number :::");
+        System.out.println("Input number: ");
+        System.out.println("Output is: ");
+        AbstractMath.roundedNumber(option);
+    }
+
+    static void countDistinct(Scanner input) {
+        System.out.println("::: Reverse a number :::");
+        System.out.println("Input number: ");
+        System.out.println("Output is: ");
+        AbstractMath.countDistinct();
+    }
+
+    static void decimalToBinary(Scanner input) {
+        System.out.println("::: Reverse a number :::");
+        System.out.println("Input number: ");
+        System.out.println("Output is: ");
+        AbstractMath.decimalToBinary(option);
+
+    }
+
 }
